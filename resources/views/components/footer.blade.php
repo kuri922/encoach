a70d5fafb5fc4f9f2ea4236b7733aad83c82eb8e
栗山雅史 <kuri.kuri922@gmail.com>
2021-11-02 18:47:31 +0900

    フッター実装

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,5 @@
+
+ <nav class="navbar navbar-expand-md navbar-light shadow-sm fixed-bottom mt-5 footer-container">
+    <a class="navbar-brand mx-auto" href="{{ url('/') }}">
+    {{ config('app.name', 'Laravel') }}
+</nav>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,6 +34,8 @@
         <main class="py-4 mb-5">
             @yield('content')
         </main>
+        @component('components.footer')
+         @endcomponent
     </div>
 </body>
 </html>
