bfd5e01fa04aed6a3f5583da76061e62359ae9c3
栗山雅史 <kuri.kuri922@gmail.com>
2021-11-02 13:51:53 +0900

    ログイン機能

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -6,7 +6,7 @@
 <div class="d-flex justify-content-center">
     <div class="row w-75">
         <div class="col-5 offset-1">
-            <img src="{{ asset('img/dummy.png')}}" class="w-100 img-fuild">
+            <img src="{{ asset('img/dummy.jpg')}}" class="w-100 img-fuild">
         </div>
         <div class="col">
             <div class="d-flex flex-column">
@@ -34,17 +34,17 @@
                 <input type="hidden" name="name" value="{{$product->name}}">
                 <input type="hidden" name="price" value="{{$product->price}}">
                 <div class="form-group row">
-                    <label for="quantity" class="col-sm-2 col-form-label">数量</label>
+                    
                     <div class="col-sm-10">
-                        <input type="number" id="quantity" name="qty" min="1" value="1" class="form-control w-25">
+                        <!-- <input type="number" id="quantity" name="qty" min="1" value="1" class="form-control w-25"> -->
                     </div>
                 </div>
                 <input type="hidden" name="weight" value="0">
                 <div class="row">
                     <div class="col-7">
                         <button type="submit" class="btn encoach-submit-button w-100">
-                            <i class="fas fa-shopping-cart"></i>
-                            カートに追加
+                            <!-- <i class="fas fa-shopping-cart"></i> -->
+                            申し込み
                         </button>
                     </div>
                     <div class="col-5">
@@ -61,12 +61,7 @@
                 {{$product->description}}
           </p>
 
-        <div class="offset-1 col-11">
-            <hr class="w-100">
-            <h3 class="float-left">カスタマーレビュー</h3>
-        </div>
-
-        <div class="offset-1 col-10">
+        <!-- <div class="offset-1 col-10"> -->
             <!-- レビューを実装する箇所になります -->
         </div>
     </div>
