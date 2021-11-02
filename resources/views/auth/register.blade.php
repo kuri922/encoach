c87eaec2bd0e673ed7e624e1eafc571902fc0288
栗山雅史 <kuri.kuri922@gmail.com>
2021-11-02 10:11:01 +0900

    create_login

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,97 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-5">
+            <h3 class="mt-3 mb-3">新規会員登録</h3>
+
+            <hr>
+
+            <form method="POST" action="{{ route('register') }}">
+              @csrf
+
+                <div class="form-group row">
+                    <label for="name" class="col-md-5 col-form-label text-md-left">氏名<span class="ml-1 encoach-require-input-label"><span class="encoach-require-input-label-text">必須</span></span></label>
+
+                    <div class="col-md-7">
+                        <input id="name" type="text" class="form-control @error('name') is-invalid @enderror encoach-login-input" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus placeholder="侍 太郎">
+
+                        @error('name')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>氏名を入力してください</strong>
+                        </span>
+                        @enderror
+                    </div>
+                </div>
+
+                <div class="form-group row">
+                    <label for="email" class="col-md-5 col-form-label text-md-left">メールアドレス<span class="ml-1 encoach-require-input-label"><span class="encoach-require-input-label-text">必須</span></span></label>
+
+                    <div class="col-md-7">
+                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror encoach-login-input" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="encoach@encoach.com">
+
+                        @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>メールアドレスを入力してください</strong>
+                        </span>
+                        @enderror
+                    </div>
+                </div>
+
+                <div class="form-group row">
+                    <label for="password" class="col-md-5 col-form-label text-md-left">郵便番号<span class="ml-1 encoach-require-input-label"><span class="encoach-require-input-label-text">必須</span></span></label>
+
+                    <div class="col-md-7">
+                        <input type="text" class="form-control @error('postal_code') is-invalid @enderror encoach-login-input" name="postal_code" required placeholder="564-0043">
+                    </div>
+                </div>
+
+                <div class="form-group row">
+                    <label for="password" class="col-md-5 col-form-label text-md-left">住所<span class="ml-1 encoach-require-input-label"><span class="encoach-require-input-label-text">必須</span></span></label>
+
+                    <div class="col-md-7">
+                        <input type="text" class="form-control @error('address') is-invalid @enderror encoach-login-input" name="address" required placeholder="大阪府吹田市垂水町3-20-27">
+                    </div>
+                </div>
+
+                <div class="form-group row">
+                    <label for="password" class="col-md-5 col-form-label text-md-left">電話番号<span class="ml-1 encoach-require-input-label"><span class="encoach-require-input-label-text">必須</span></span></label>
+
+                    <div class="col-md-7">
+                        <input type="text" class="form-control @error('phone') is-invalid @enderror encoach-login-input" name="phone" required placeholder="06-5790-9039">
+                    </div>
+                </div>
+
+                <div class="form-group row">
+                    <label for="password" class="col-md-5 col-form-label text-md-left">パスワード<span class="ml-1 encoach-require-input-label"><span class="encoach-require-input-label-text">必須</span></span></label>
+
+                    <div class="col-md-7">
+                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror encoach-login-input" name="password" required autocomplete="new-password">
+
+                        @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                    </div>
+                </div>
+
+                <div class="form-group row">
+                    <label for="password-confirm" class="col-md-5 col-form-label text-md-left"></label>
+
+                    <div class="col-md-7">
+                        <input id="password-confirm" type="password" class="form-control encoach-login-input" name="password_confirmation" required autocomplete="new-password">
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <button type="submit" class="btn encoach-submit-button w-100">
+                        アカウント作成
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+@endsection
